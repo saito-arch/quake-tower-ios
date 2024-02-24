41e4d19394456f3f4d4bbda940f38e8d12624d2e
saito <saito.arch@gmail.com>
2024-02-24 18:49:36 +0900

    Add log to Common

---
+++ QuakeTower/Service/Domain/Models/Session.swift
@@ -0,0 +1,8 @@
+//
+//  Session.swift
+//  QuakeTower
+//
+//  Created by Saito on 2024/02/24.
+//
+
+import Foundation
--- QuakeTower/System/Common.swift
+++ QuakeTower/System/Common.swift
@@ -13,3 +13,18 @@ let IS_TESTING = { (isTesting: String?) -> Bool in
     }
     return false
 }(ProcessInfo().environment["IS_TESTING"])
+
+func log(_ msg: String, file: String = #file, line: Int = #line, function: String = #function) {
+
+    var f = "NONE"
+
+    if let _f = file.components(separatedBy: "/").last {
+        f = _f.replacingOccurrences(of: ".swift", with: "")
+        f = f.replacingOccurrences(of: "ViewController", with: "Vc")
+        f = f.replacingOccurrences(of: "Repository", with: "Rp")
+        f = f.replacingOccurrences(of: "Interactor", with: "Ir")
+        f = f.replacingOccurrences(of: "Presenter", with: "Ps")
+    }
+
+    print("\(f):\(line) [\(function)]: \(msg)")
+}
